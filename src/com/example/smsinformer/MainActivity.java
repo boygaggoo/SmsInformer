360f13daca603428f27f3225423892747551a806
Jehy <fate@jehy.ru>
2013-03-13 14:36:36 +0400

    v 1.02
    It works!

--- src/com/example/smsinformer/MainActivity.java
+++ src/com/example/smsinformer/MainActivity.java
@@ -1,22 +1,24 @@
 package com.example.smsinformer;
 
 import java.util.Calendar;
-
 import android.app.Activity;
 import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
 
-    @Override
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
@@ -29,42 +31,59 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-public void SendSMS(View view) 
-{
-	//test sending SMS
-	SmsManager smsManager = SmsManager.getDefault();
-	String phone="+79161234567";
-	smsManager.sendTextMessage(phone, null, "непереводимая игра слов", null, null);
+	public void SendSMS(View view) {
+		// test sending SMS
+
+		String phone=((EditText)findViewById(R.id.phone)).getText().toString();
+		String msg=((EditText)findViewById(R.id.msg)).getText().toString();
+		SmsManager smsManager = SmsManager.getDefault();
+		//phone = "+79161234567";
+		//msg="непереводимая игра слов";
+		
+		smsManager.sendTextMessage(phone, null, msg,
+				null, null);
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+		// Add the buttons
+		builder.setMessage("Сообщение отослано").setTitle("Ок");
+		builder.setPositiveButton("Ок", new DialogInterface.OnClickListener() {
+
+			public void onClick(DialogInterface dialog, int id) {
+				// MainActivity.this.finish();
+				// User clicked OK button
+			}
+		});
+		builder.show();
+	}
+
+	public void ToggleService(View view) {
+
+		boolean on = ((ToggleButton) view).isChecked();
+		AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+		Intent intent = new Intent(this, ServiceExample.class);
+	   // Intent intent=new Intent("com.example.smsinformer.ServiceExample");
+		PendingIntent pintent = PendingIntent.getService(this, 0,
+				intent, 0);
+
+		if (on) {
+			// Enable vibrate
+			//startService(new Intent(this, ServiceExample.class));
+		    //this.startService(intent);
+			// inetnt=new Intent(this,ServiceExample.class);
+			Log.v("SmsInformer", "Starting service");
+			// startService(inetnt);
+			Calendar cal = Calendar.getInstance();
+			// Start every 30 seconds
+			alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+					20 * 1000, pintent);
+
+		} else {
+			// Disable vibrate
+			// inetnt=new Intent(this,ServiceExample.class);
+			Log.v("SmsInformer", "Stopping service");
+			// stopService(inetnt);
+			alarm.cancel(pintent);
+		}
+
+	}
 }
-
-public void ToggleService(View view) 
-{
-
-    boolean on = ((ToggleButton) view).isChecked();
-        AlarmManager alarm = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(this, ServiceExample.class);
-        PendingIntent pintent = PendingIntent.getService(this, 0, intent, 0);
-    
-    if (on) {
-        // Enable vibrate
-
-        //inetnt=new Intent(this,ServiceExample.class);
-        Log.v("SmsInformer","Starting service");
-        //startService(inetnt);
-        Calendar cal = Calendar.getInstance();
-        // Start every 30 seconds
-        alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), 30*1000, pintent); 
-        
-        
-    } else {
-        // Disable vibrate
-        //inetnt=new Intent(this,ServiceExample.class);
-        Log.v("SmsInformer","Stopping service");
-        //stopService(inetnt);
-        alarm.cancel(pintent);
-    }
-
-}
-}
-
-
--- src/com/example/smsinformer/ServiceExample.java
+++ src/com/example/smsinformer/ServiceExample.java
@@ -1,73 +1,199 @@
 package com.example.smsinformer;
 
-import android.app.Service;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import android.app.AlarmManager;
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Environment;
 import android.os.IBinder;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.Toast;
 
-public class ServiceExample extends Service {
+public class ServiceExample extends IntentService {
+	public ServiceExample(String name) {
+		super(name);
+		Log.e("Service create", "creating service");
+		// TODO Auto-generated constructor stub
+	}
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
+	public ServiceExample() {
+		super("com.example.smsinformer.ServiceExample");
+		Log.e("Service create", "creating service");
+		// TODO Auto-generated constructor stub
+	}
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Toast.makeText(this,"Service Created",Toast.LENGTH_LONG).show();
-    }
+	ArrayList<String> receivers = new ArrayList<String>();
+	ArrayList<String> messages = new ArrayList<String>();
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this,"Service Destroy",Toast.LENGTH_LONG).show();
-    }
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
 
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        Toast.makeText(this,"Service LowMemory",Toast.LENGTH_LONG).show();
-    }
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Toast.makeText(this, "Service Created", Toast.LENGTH_LONG).show();
 
-    @Override
-    public void onStart(Intent intent, int startId) {
-        super.onStart(intent, startId);
-        Toast.makeText(this,"Service start",Toast.LENGTH_LONG).show();
-    }
+		Log.v("Thread service", "Creating service");
+		Log.v("Thread service", "Reading receivers file");
+		String path = Environment.getExternalStorageDirectory()
+				+ "/SmsInformer/receivers.txt";
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+		BufferedReader rd;
+		// final StringBuffer storedString = new StringBuffer();
 
-        Log.v("Service","Starting");
-        Toast.makeText(this,"task perform in service",Toast.LENGTH_LONG).show();
-        ThreadDemo td=new ThreadDemo();
-        td.start();
-        return super.onStartCommand(intent, flags, startId);
-    }
-    
-    private class ThreadDemo extends Thread{
-        @Override
-        public void run() {
-            super.run();
-            try{
-            	//while(true)
-            	//{
-		            Log.v("Thread service","all ok");
-		            //handler.sendEmptyMessage(0);
-            	//}
-            }catch(Exception e){
-                e.getMessage();
-            }
-        }
-    }
-    /*
-   private Handler handler=new Handler(){
-    @Override
-    public void handleMessage(Message msg) {
-        super.handleMessage(msg);
-        //showAppNotification();
-    }
-   };*/
+		try {
+			rd = new BufferedReader(new FileReader(path));
+			String line = null;
+			// read all the lines till the end
+			while ((line = rd.readLine()) != null) {
+				receivers.add(line);
+			}
+			rd.close(); // close reader
+
+			rd.close(); // close reader
+
+		} catch (Exception e) {
+			Log.e("Service run error", e.getMessage());
+			Log.e("Service run", "Stopping service (harakiri)");
+			AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+			Intent intent = new Intent(this, ServiceExample.class);
+			PendingIntent pintent = PendingIntent
+					.getService(this, 0, intent, 0);
+			alarm.cancel(pintent);
+		}
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Toast.makeText(this, "Service Destroy", Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public void onLowMemory() {
+		super.onLowMemory();
+		Toast.makeText(this, "Service LowMemory", Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public void onStart(Intent intent, int startId) {
+		super.onStart(intent, startId);
+		Toast.makeText(this, "Service start", Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+
+		Log.v("Service", "Starting");
+		Toast.makeText(this, "task perform in service", Toast.LENGTH_LONG)
+				.show();
+		ThreadDemo td = new ThreadDemo();
+		td.start();
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	private class ThreadDemo extends Thread {
+		@Override
+		public void run() {
+			super.run();
+			// while(true)
+			// {
+			Log.v("Thread service", "Running service thread");
+			boolean s = ReadMessages();
+			if (!s)
+				Log.v("Thread service", "ReadMessages call failed, aborting");
+			else if (messages.size() == 0)
+				Log.v("Thread service", "No messages, abotring");
+			else if (receivers.size() == 0)
+				Log.v("Thread service", "No receivers, abotring");
+			else {
+				SmsManager smsManager = SmsManager.getDefault();
+				//ArrayList<String> msgTexts = null;
+				//ArrayList<PendingIntent> listOfIntents = null;
+				//String SENT = "SMS_SENT";
+				for (int i = 0; i < messages.size(); i++)
+					for (int j = 0; j < receivers.size(); j++) {
+						Log.v("Thread service",
+								"Sending sms to " + receivers.get(j));
+						String message = messages.get(i);
+
+						/*boolean bBigSMS = message.length() > 160;
+
+						if (bBigSMS) {
+							msgTexts = smsManager.divideMessage(message);
+
+							listOfIntents = new ArrayList<PendingIntent>();
+							for (int k = 0; k < msgTexts.size(); k++) {
+
+								Intent sentIntent = new Intent(SENT);
+
+								PendingIntent pi = PendingIntent.getBroadcast(
+										null, 0, sentIntent, 0);
+								listOfIntents.add(pi);
+							}
+						}*/
+						String phone = receivers.get(j);
+						//smsManager.sendMultipartTextMessage(phone, null,
+						//		msgTexts, listOfIntents, null);
+						 smsManager.sendTextMessage(phone,
+						 null,message
+						 , null, null);
+					}
+			}
+			messages.clear();
+			// handler.sendEmptyMessage(0);
+			// }
+
+		}
+
+		boolean ReadMessages() {
+
+			messages.clear();
+			String path = Environment.getExternalStorageDirectory()
+					+ "/SmsInformer/messages.txt";
+			BufferedReader rd;
+			// final StringBuffer storedString = new StringBuffer();
+
+			try {
+				rd = new BufferedReader(new FileReader(path));
+				String line = null;
+				// read all the lines till the end
+				while ((line = rd.readLine()) != null) {
+					messages.add(line);
+				}
+				rd.close(); // close reader
+
+				rd.close(); // close reader
+
+				File file = new File(path);
+				file.delete();
+				return true;
+			} catch (Exception e) {
+				Log.e("Service run", e.getMessage());
+				messages.clear();
+				return false;
+			}
+		}
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		Log.v("Thread service", "handling intent");
+	}
+
+	/*
+	 * private Handler handler=new Handler(){
+	 * 
+	 * @Override public void handleMessage(Message msg) {
+	 * super.handleMessage(msg); //showAppNotification(); } };
+	 */
 }
