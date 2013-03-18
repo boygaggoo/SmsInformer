a9a0d8dd8f5d7f4cc59f9fd27e79fb5813efda85
Jehy <fate@jehy.ru>
2013-03-19 01:15:38 +0400

    add date to SMS,  1_005

--- src/com/example/smsinformer/CentreonMessage.java
+++ src/com/example/smsinformer/CentreonMessage.java
@@ -1,10 +1,24 @@
 package com.example.smsinformer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
 public class CentreonMessage {
 
 	String type,service_description,host,host_alias,address,state,phone;
 	int time;
 	String getSmsText()
 	{
-		return type+": "+service_description+" on "+host_alias+"("+address+") is "+state;
+		String format="dd/MM HH:mm";
+		Date dt = new java.util.Date(time);
+		SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.UK);
+		sdf.setTimeZone(TimeZone.getTimeZone("Europe/Moscow"));
+		String dt2=sdf.format(dt);
+		String message= type+": "+service_description+" on "+host_alias+"("+address+") is "+state +" on "+dt2;
+		if(message.length()>160)
+			message=message.substring(0,157)+"...";//cut message
+		return message;
 	}
 }
